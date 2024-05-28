61187cee5e0c8cc38dca04c91f6c1d737dd4f070
RedDeadth <mimamamemima335@gmail.com>
2024-05-28 11:55:01 -0500

    Añadí un ruteo a web.php
    Añadí una ruta para reservas donde el usuario podra cancelar y modificar sus reservas

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::resource('reservas', ReservaController::class)
+    ->only(['index', 'create', 'store', 'edit', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
